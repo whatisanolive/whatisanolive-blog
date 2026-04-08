acc2090116cbf880e8fa0a13b11e62b80cca638a
Ishita Khandelwal <whatisanolive@Ishitas-MacBook-Air.local>
2026-04-08 21:10:34 +0530

    modified next.config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+
   images: {
     remotePatterns: [
       {
